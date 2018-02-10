0fed91f20ebf4290e51ed8cdb3ac52e371082e78
guybenshoshan <airplane>
2018-02-10 11:32:25 +0200

    added share to social networks in the footer

---
+++ src/constants/social.ts
@@ -0,0 +1,23 @@
+import {BASE_URL} from "./global";
+
+export const SOCIAL_NETWORKS = {
+  facebook: {
+    fbUrl: "https://www.facebook.com/sharer/sharer.php",
+    websiteUrl: BASE_URL,
+    messageTitle: "Crypto Board",
+    message: "Crypto Currencies Dashboard (Powered by CoinMarketCap)"
+  },
+  twitter: {
+    twrUrl: "https://twitter.com/intent/tweet",
+    message: BASE_URL + " - Crypto Currencies Dashboard (Powered by CoinMarketCap)"
+  },
+  whatsapp: {
+    waUrl: "whatsapp://send",
+    message: BASE_URL
+  },
+  telegram: {
+    tgUrl: "https://telegram.me/share/url",
+    websiteUrl: BASE_URL,
+    message: "Crypto Currencies Dashboard (Powered by CoinMarketCap)"
+  }
+}
