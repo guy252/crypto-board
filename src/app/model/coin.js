293745c93aaafa4dd23c27dfa3f2334d45872430
guybenshoshan <airplane>
2018-02-10 13:26:22 +0200

    removed compiled files from git - app root folder

--- src/app/model/coin.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Coin = /** @class */ (function () {
-    function Coin() {
-    }
-    return Coin;
-}());
-exports.Coin = Coin;
-//# sourceMappingURL=coin.js.map
