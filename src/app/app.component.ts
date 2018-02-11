ceda75ca0d20652ecedf5376c6252b92446b933d
guybenshoshan <airplane>
2018-02-11 21:16:11 +0200

    moved template and styles to dedicated file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,21 +3,10 @@ import {Angulartics2GoogleAnalytics} from 'angulartics2/ga';
 
 @Component({
   selector: 'my-app',
-  template: `
-    <header-navbar></header-navbar>
-    <div class="container">
-      <router-outlet></router-outlet>
-    </div>
-    <footer-navbar></footer-navbar>
-  `,
-  styles: [`
-    .container {
-      border: 0px solid;
-      margin-top: 1.2rem;
-    }
-  `]
-
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
+
 export class AppComponent {
   constructor(angulartics2GoogleAnalytics: Angulartics2GoogleAnalytics) {
 
