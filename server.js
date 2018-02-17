6f2081f4195001bc906884cfb2b800fbf228269e
guybenshoshan <airplane>
2018-02-17 19:28:22 +0200

    express - redirect to root

--- server.js
+++ server.js
@@ -11,5 +11,9 @@ const app = express();
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist'));
 
+app.get('/dashboard', function(req, res, next){
+  res.redirect('/');
+})
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
