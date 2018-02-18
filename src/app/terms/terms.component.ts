aa8fb6dfc7aeda715090e7255c1edd18f9c5e5e5
guybenshoshan <airplane>
2018-02-18 19:44:28 +0200

    added files from previous commit. added keywords. changed google analytics to production.

---
+++ src/app/terms/terms.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'terms',
+  templateUrl: './terms.component.html',
+  styleUrls: ['./terms.component.css']
+})
+export class TermsComponent {
+  modalId = 'termsModal';
+
+  constructor() {
+  }
+}
